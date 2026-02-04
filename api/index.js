ddc61b12333ad928fea38258abd789236b33da7f
Rodrigo Lopes <gestor.rodrigolopes@gmail.com>
2026-02-04 13:17:24 -0300

    Fix: Revert to api/index.js (ESM) to resolve Vercel routing issue

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 // Vercel Serverless Function Wrapper
 // This file wraps the Express app for Vercel's serverless environment
 
-const app = require('../backend/src/server.js');
+import app from '../backend/src/server.js';
 
 // Export the Express app as a serverless function
-module.exports = app;
+export default app;
